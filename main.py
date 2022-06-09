b0c1fe05e200cb2578e26e4bfac203d60a70e344
Virenth <virenth@gmail.com>
2022-06-09 18:03:13 +0300

    Second version

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 from flask import Flask
 from utils import load_candidtates
 
-candidates_data = load_candidtates()
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def print_candidates_main():
-    print_candidates_string = ""
+    candidates_data = load_candidtates()
+    candidate_string = "<pre>"
     for person in candidates_data:
-        candidate_string = str(person['name']) + " " + str(person['position']) + " " + str(person['skills'] + " ")
-        print_candidates_string += candidate_string     #у меня не получается вставить \n в текст
-    return print_candidates_string
+        candidate_string += f"""
+        {person['name']}\n
+        {person['position']}\n
+        {person['skills']}\n
+        """
+    candidate_string += "</pre>"
+    return candidate_string
 
 
-@app.route("/candidates/<int:cand_id>") #не получается использовать <int:cand_id>
+@app.route("/candidates/<int:cand_id>")
 def print_candidates_uid(cand_id):
-    print_candidates_string = ""
+    candidates_data = load_candidtates()
+    candidate_string = "<pre>"
     for person in candidates_data:
-        if int(person[id]) == cand_id:
-            candidate_string = str(person['picture']) + " " + str(person['name']) + " " + str(person['position']) + " " + str(person['skills'] + " ")
-            print_candidates_string += candidate_string
+        if int(person['id']) == int(cand_id):
+            candidate_string += f"""
+            <img src="{person['picture']}">\n
+            {person['name']}\n
+            {person['position']}\n
+            {person['skills']}\n
+            """
+            candidate_string += "</pre>"
         else:
             pass
-    return print_candidates_string
+    return candidate_string
 
 
-@app.route("/skills/<cand_skill>")  # не получается использовать <cand_skill>
+@app.route("/skills/<cand_skill>")
 def print_candidates_skill(cand_skill):
-    print_candidates_string = ""
+    candidates_data = load_candidtates()
+    candidate_string = "<pre>"
     for person in candidates_data:
-        if cand_skill in person['skills']:
-            candidate_string = str(person['name']) + " " + str(person['position']) + " " + str(person['skills'] + " ")
-            print_candidates_string += candidate_string
+        if cand_skill in person['skills'].lower().split(", "):
+            candidate_string += f"""
+            {person['name']}\n
+            {person['position']}\n
+            {person['skills']}\n
+            """
         else:
             pass
-    return print_candidates_string
-
-/skills/<x>
-
-
-@app.route("/feed/")
-def test_print():
-    return f"{candidates_data}"
+    candidate_string += "</pre>"
+    return candidate_string
 
 app.run()
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import json
 
-def load_candidtates():
+def load_candidtates() -> list[dict]:
     """
     Читает файл candidates.json, возвращает как список словарей candidates_data
     """
-    with open("candidates.json") as file:
-        candidates_data = json.loads(file.read())
+    with open("candidates.json", "r", encoding="utf-8") as file:
+        candidates_data = json.load(file)
     return candidates_data
